5ca1aaec174037929c6da68dd14c5f3d66ce1289
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-08-19 09:59:43 +0200

    add express.Router, secrete routes to folders, add start script

--- server.js
+++ server.js
@@ -5,91 +5,22 @@ const db = require('./db');
 
 const app = express();
 
+// import routes
+const testimonialRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use('/api', testimonialRoutes); // add testimonial routes to server
+app.use('/api', concertsRoutes); // add concerts routes to server
+app.use('/api', seatsRoutes); // add seats routes to server
 
-// get all db.testimonials array
-app.get('/testimonials', (req, res) => {
-    res.json(db.testimonials);
-  });
-
-// get random db.testimonials array element
-app.get('/testimonials/random', (req, res) => {
-    const randomIndex = Math.floor(Math.random() * db.testimonials.length);
-    const randomTestimonial = db.testimonials[randomIndex];
-    res.json(randomTestimonial);
-});
-
-// get single db.testimonials array element
-app.get('/testimonials/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const testimonial = db.testimonials.find((item) => item.id === id);
-    if (testimonial) {
-        res.json(testimonial);
-    } else {
-        res.status(404).json({ error: 'Testimonial not found' });
-    }
-});
-
-
-// add new element to an array, Id must be random
-app.post('/testimonials/:id', (req, res) => {
-
-    const { author, text } = req.body;
-
-    if (!author || !text) {
-        return res.status(400).json({ error: 'Author and text are required fields' });
-      }
-
-    const id = uuidv4();
-    const newTestimonial = { id, author, text };
-
-    db.testimonials.push(newTestimonial);
-
-});
-
-// modify atributes in array element with matching Id
-app.put('/testimonials/:id', (req, res) => {
-    const { id } = req.params;
-    const { author, text } = req.body;
-
-    if (!author || !text) {
-        return res.status(400).json({ error: 'Author and text are required fields' });
-      }
-
-    const testimonial = db.testimonials.find((item) => item.id === id);
-
-    if (!testimonial) {
-      return res.status(404).json({ message: 'Testimonial not found' });
-    }
-
-    testimonial.author = author;
-    testimonial.text = text;
-
-    res.json({ message: 'OK' });
-});
-
-// delete element from db.testimonials array
-app.delete('/testimonials/:id', (req, res) => {
-    const testimonialId = req.params.id;
-
-    const testimonialIndex = db.testimonials.findIndex((testimonial) => testimonial.id === testimonialId);
-
-    if (testimonialIndex !== -1) {
-      db.testimonials.splice(testimonialIndex, 1);
-      res.json({ message: 'OK' });
-    } else {
-      res.status(404).json({ message: 'Testimonial not found' });
-    }
-  });
-
-
+// catching bad links
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
+  res.status(404).json({ message: 'Not found...' });
 });
-
 app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
+  console.log('Server is running on port: 8000');
 });
