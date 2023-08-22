c367fb0967c31f354910c69a97e6ead94a723b7a
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-08-22 12:22:17 +0200

    Prepare project for Replit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const db = require('./db');
+const path = require('path');
 
 const app = express();
 
@@ -13,14 +14,26 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonialRoutes); // add testimonial routes to server
 app.use('/api', concertsRoutes); // add concerts routes to server
 app.use('/api', seatsRoutes); // add seats routes to server
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // catching bad links
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
-app.listen(8000, () => {
+
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
